2204bb689a5c83beca5ef474130d0ba29167a4bf
Kevin Muchene <muchenekevin@gmail.com>
2022-11-03 14:47:20 -0500

    Initial commit for User entity

---
+++ src/main/java/com/example/labs/domain/User.java
@@ -0,0 +1,16 @@
+package com.example.labs.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    long id;
+    String name;
+    List<Post> posts;
+}
