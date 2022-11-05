acb255816dd40c3961d1d433059fc5650649de23
Kevin Muchene <muchenekevin@gmail.com>
2022-11-05 18:25:10 -0500

    Created saveComments() method

--- src/main/java/com/example/labs/service/PostService.java
+++ src/main/java/com/example/labs/service/PostService.java
@@ -1,7 +1,9 @@
 package com.example.labs.service;
 
 
+import com.example.labs.domain.Comment;
 import com.example.labs.domain.Post;
+import com.example.labs.domain.dto.response.CommentDto;
 import com.example.labs.domain.dto.response.PostDto;
 
 import java.util.List;
@@ -15,6 +17,8 @@ public interface PostService {
     void deleteById(int id);
 
     void save(PostDto postDto);
+
+    void saveComments(Integer id, Comment comment);
 
 
 }
