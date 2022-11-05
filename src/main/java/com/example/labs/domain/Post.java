b1b27185f44d242d04bd3f4c78fd7818c03a7dab
Kevin Muchene <muchenekevin@gmail.com>
2022-11-05 14:18:51 -0500

    Added JPA annotations for Id and GeneratedValue

--- src/main/java/com/example/labs/domain/Post.java
+++ src/main/java/com/example/labs/domain/Post.java
@@ -1,15 +1,19 @@
 package com.example.labs.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import javax.persistence.*;
 
 @Data
+
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Post {
 
-    long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
     String title;
     String content;
     String author;
