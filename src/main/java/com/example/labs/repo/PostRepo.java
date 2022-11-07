577631611c676ba9cd4de900af30130f16866300
Kevin Muchene <muchenekevin@gmail.com>
2022-11-07 12:25:41 -0600

    Queries for getting users with more than n posts and getting posts matching by titles

--- src/main/java/com/example/labs/repo/PostRepo.java
+++ src/main/java/com/example/labs/repo/PostRepo.java
@@ -1,9 +1,19 @@
 package com.example.labs.repo;
 
 import com.example.labs.domain.Post;
+import com.example.labs.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
-public interface PostRepo extends JpaRepository<Post, Integer> { }
+public interface PostRepo extends JpaRepository<Post, Integer> {
+    @Query("select u from User u where u.posts.size > :n")
+    List<User> userWithMoreThanNPost(int n);
+
+    @Query("select post from Post post where post.title = :title")
+    List<Post> getPostByTitleMatches(String title);
+}
