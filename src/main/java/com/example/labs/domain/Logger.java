bcf4344f70d886a40f952c945408e8050d8edefe
Kevin Muchene <muchenekevin@gmail.com>
2022-11-09 14:26:34 -0600

    Changed class fields to match appropriate lab requirements

--- src/main/java/com/example/labs/domain/Logger.java
+++ src/main/java/com/example/labs/domain/Logger.java
@@ -1,26 +1,26 @@
 package com.example.labs.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Builder
 public class Logger {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String transactionalId;
-    private Date date;
-    private Date time;
+    private Long transactionalId;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateAndTime;
     private String principle;
     private String operation;
 
