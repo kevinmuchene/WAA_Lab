084d1ed25ebc6225a5f28fa47b8ea4240c8dbe76
Kevin Muchene <muchenekevin@gmail.com>
2022-11-09 14:28:38 -0600

    Re-assigned class duties

--- src/main/java/com/example/labs/aspect/LoggerAspect.java
+++ src/main/java/com/example/labs/aspect/LoggerAspect.java
@@ -1,27 +1,22 @@
 package com.example.labs.aspect;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+import com.example.labs.domain.Logger;
+import com.example.labs.repo.LoggerRepo;
+import org.aspectj.lang.JoinPoint;
 
-@Aspect
-@Component
+import java.util.Date;
+
 public class LoggerAspect {
 
-    Logger logger = LoggerFactory.getLogger(LoggerAspect.class);
+    private LoggerRepo loggerRepo;
 
-    @Around("@annotation(com.example.labs.aspect.ExecutionTime)")
-    public Object executionTime(
-        ProceedingJoinPoint proceedingJoinPoint
-    ) throws Throwable {
-        long startTime = System.currentTimeMillis();
-        Object obj = proceedingJoinPoint.proceed();
-        long endTime = System.currentTimeMillis();
-        logger.info("Method name" + proceedingJoinPoint.getSignature() + " time taken to execute " + (endTime - startTime));
-        return obj;
+    public void logging(JoinPoint joinPoint){
+        var log = Logger.builder()
+                .dateAndTime(new Date())
+                .principle("Kevin")
+                .operation(joinPoint.getSignature().getName())
+                .build();
+
+        loggerRepo.save(log);
     }
 }
